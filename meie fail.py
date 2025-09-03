1f124ca9367a73fab3220ac07f7333cd0ed41382
Lvakos <foxigosi222@gmail.com>
2025-09-03 11:27:32 +0300

    Add files via upload

--- meie fail.py
+++ meie fail.py
@@ -36,3 +36,34 @@ umbermot = 2 * (pikkus + laius)
 pindala = pikkus * laius
 print("ümbermõõt: ", umbermot)
 print("laius: ", laius)
+
+#kirjuta programm mis küsib kasutajalt järgmised andmed
+# nimi
+# vanus
+ #elukoht(linn)
+ #lemmikfilm
+ #lemmikmuusik
+# kolm lemmiktoitu
+ 
+#1. salvesta iga sastus eraldi muutujasse
+#2. väljesta tervitustekst, kus kasutakse nimi ja vanus
+#3. väljesta lause elukoha kohta
+#4. väljesta lause lemmikfilmi kohta ja muusika kohta
+#5. väljesta kõik toidud ühes lauses. (Minu lemmiktoidud on ?,? ja ?)
+print("=================================================================================")
+
+nimi = input("What is your name?: ")
+vanus = input("How old are you?: ")
+elukoht = input("Where do you live? (City): ")
+lemmikfilm = input("What your favourite movie?: ")
+lemmikmuusik = input("What yours favourite song? ")
+lemmiktoitu = input("What's your three favourite food?(Name only food, (burger, hotdog and pizza): ")
+
+print("=================================================================================")
+print("Welcome ", nimi)
+print("You're ", vanus + " years old")
+print("You're living in ", elukoht)
+print("Your favourite movie is: ", lemmikfilm)
+print("Your favourite song is: ", lemmikmuusik)
+print("And your three favourite food are: ", lemmiktoitu)
+print("Done!")
